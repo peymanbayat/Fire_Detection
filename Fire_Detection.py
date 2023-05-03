77a45c23c67ceacb5bdc305018c0ba19e55bd09c
peymanbayat <53484770+peymanbayat@users.noreply.github.com>
2023-05-03 14:44:04 +0330

    Update Fire_Detection.py

--- Fire_Detection.py
+++ Fire_Detection.py
@@ -50,7 +50,7 @@ if app_mode == 'About App':
 
   🔗 Links
 [![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://antrosafin.netlify.app)
-[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://linkedin.com/in/antro-safin-m)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://ir.linkedin.com/in/dr-peyman-bayat)
 [![twitter](https://img.shields.io/badge/Github-1DA1F2?style=for-the-badge&logo=github&logoColor=white)](https://github.com/peymanbayat)
 """)   
 
